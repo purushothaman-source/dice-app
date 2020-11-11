ce154b7faadd14986580d2c357d900e4bb424ce9
purushothaman-source <purushoth99jun@gmail.com>
2020-11-11 09:51:00 +0530

    Added:second face logic

--- dice.sh
+++ dice.sh
@@ -3,6 +3,9 @@ random=$(( RANDOM%6+1 ))
 case $random in
           1)echo "first face "
            ;;
+          2)echo "second face "
+           ;;
+
          *)echo "not amoung 1 to 6"
           ;;
 esac
